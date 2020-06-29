4443f7cf5770b4def38fa0e7a3534d8a5763adde
verissteph <stephanieverissimopp@gmail.com>
2020-06-29 00:47:05 -0300

    Validação com mensagem de erro Ok

--- login/js/app.js
+++ login/js/app.js
@@ -1,5 +1,5 @@
 let formulario = document.getElementById('formulario');
-let caixaDeErros = document.querySelector('.caixa-erro');
+let caixaDeErros = document.querySelector('#caixa-erro');
 let listaDeErros = document.querySelector('.lista-erro');
 let erro = document.querySelectorAll('.lista-erro li');
 let email = document.getElementById('email');
@@ -10,11 +10,17 @@ formulario.addEventListener("submit", function (e) {
     e.preventDefault();
     if (email.value == '') {
         listaDeErros.innerHTML += "<li>" + " Campo email não foi preenchido" + "</li>";
+        // email.style.border = '1px solid #bb4430';
     }
 
     if (senha.value == '') {
         listaDeErros.innerHTML += "<li>" + " Campo senha não foi preenchido" + "</li>";
+        // senha.style.border = '1px solid #bb4430';
+
     } else if (senha.value.length < 7 && senha.value !== '') {
-        listaDeErros.innerHTML += "<li>" + "A senha deve ter no mínimo 6 caracteres";
+        listaDeErros.innerHTML += "<li>" + "Deve ter no mínimo 6 caracteres";
+        // senha.style.border = '1px solid #bb4430';
     }
+
+
 });
