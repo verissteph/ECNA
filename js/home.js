f7253ae77d5dbca2fcb146172747351d8b999746
Pablo Lucena <pablodplucenahh@gmail.com>
2020-07-05 22:15:13 -0300

    api acionada teste

---
+++ js/home.js
@@ -0,0 +1,27 @@
+var burl = 'https://api.binance.com';
+var query = '/api/v1/ticker/24hr';
+
+query += '?symbol=BTCUSDT';
+
+var url = burl + query;
+
+var ourRequest = new XMLHttpRequest();
+
+ourRequest.open('GET',url,true);
+
+ourRequest.onload = function () {
+  const obj = JSON.parse(ourRequest.responseText);
+  var btcLastPrice = parseFloat(obj.lastPrice).toFixed(2);
+
+  preco = document.getElementById('precobtcbrl')
+  preco.innerHTML = 'U$' + btcLastPrice
+
+  // console.log(ourRequest.responseText);
+}
+
+ourRequest.send()
+
+setInterval(function() {
+    ourRequest.open('GET',url,true);
+    ourRequest.send()
+}, 1000);
