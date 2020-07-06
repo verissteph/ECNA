d85689514fb566e9b7b1b5ef276193f49ca20243
Rodrigo <rodrigocosta47@outlook.com>
2020-07-06 15:53:12 -0300

    Adiciei as informações da api, agora o site puxa a volume transacionado, max e min na home e responsive

--- js/home.js
+++ js/home.js
@@ -13,13 +13,42 @@ ourRequest.onload = function () {
   const obj = JSON.parse(ourRequest.responseText);
   var btcLastPrice = parseFloat(obj.lastPrice).toFixed(2);
 
+  // preco
+
   let preco = document.getElementById("precobtcbrl")
   preco.innerHTML = 'R$' + (btcLastPrice * 5).toFixed(2)
- 
-  let precoRespontivo = document.getElementById("precobtcbrl-r")
+  let precoRespontivo = document.getElementById('precobtcbrl-r')
   precoRespontivo.innerHTML = 'R$' + (btcLastPrice * 5).toFixed(2)
 
-  // console.log(ourRequest.responseText);
+  // Mais alto
+
+  var precoMaisAlto = parseFloat(obj.highPrice).toFixed(2);
+
+  let MaisAlto = document.getElementById("maisAlto")
+  MaisAlto.innerHTML = 'R$' + (precoMaisAlto * 5).toFixed(2)
+
+  let MaisAltoR = document.getElementById("maisAlto-r")
+  MaisAltoR.innerHTML = 'R$' + (precoMaisAlto * 5).toFixed(2)
+
+  // Mais Baixo
+
+  var precoMaisBaixo = parseFloat(obj.lowPrice).toFixed(2);
+  
+  let MaisBaixo = document.getElementById("maisBaixo")
+  MaisBaixo.innerHTML = 'R$' + (precoMaisBaixo * 5).toFixed(2)
+
+  let MaisBaixoR = document.getElementById("maisBaixo-r")
+  MaisBaixoR.innerHTML = 'R$' + (precoMaisBaixo * 5).toFixed(2)
+
+  // volume    
+  var volume = parseFloat(obj.volume).toFixed(2);
+  
+  let volumeElemeto = document.getElementById("volume")
+  volumeElemeto.innerHTML = 'U$' + volume
+  
+  let volumeElemetoR = document.getElementById("volume-r")
+  volumeElemetoR.innerHTML = 'U$' + volume
+
 }
 
 ourRequest.send()
