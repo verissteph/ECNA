0c96742dc0e78d60b0ce020798d9d3140cbc2c8a
Rodrigo <rodrigocosta47@outlook.com>
2020-07-06 14:54:51 -0300

    O site está vivo

--- js/home.js
+++ js/home.js
@@ -5,22 +5,19 @@ query += '?symbol=BTCUSDT';
 
 var url = burl + query;
 
-console.log(url)
-
 var ourRequest = new XMLHttpRequest();
 
 ourRequest.open('GET',url,true);
-
-/**
- * Tentando resolver o problema no git
- */
 
 ourRequest.onload = function () {
   const obj = JSON.parse(ourRequest.responseText);
   var btcLastPrice = parseFloat(obj.lastPrice).toFixed(2);
 
-  preco = document.getElementById('precobtcbrl')
+  let preco = document.getElementById("precobtcbrl")
   preco.innerHTML = 'R$' + (btcLastPrice * 5).toFixed(2)
+ 
+  let precoRespontivo = document.getElementById("precobtcbrl-r")
+  precoRespontivo.innerHTML = 'R$' + (btcLastPrice * 5).toFixed(2)
 
   // console.log(ourRequest.responseText);
 }
