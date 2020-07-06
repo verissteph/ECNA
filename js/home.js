976a3b3c1423c9b6142a7b783b4dd81b78893e60
Rodrigo <rodrigocosta47@outlook.com>
2020-07-06 16:52:24 -0300

    Usd ta vivo

--- js/home.js
+++ js/home.js
@@ -16,29 +16,33 @@ ourRequest.onload = function () {
   // preco
 
   let preco = document.getElementById("precobtcbrl")
-  preco.innerHTML = 'R$' + (btcLastPrice * 5).toFixed(2)
+  preco.innerHTML = 'R$' + (btcLastPrice * 5.35).toFixed(2)
+  
   let precoRespontivo = document.getElementById('precobtcbrl-r')
-  precoRespontivo.innerHTML = 'R$' + (btcLastPrice * 5).toFixed(2)
+  precoRespontivo.innerHTML = 'R$' + (btcLastPrice * 5.35).toFixed(2)
+
+  let precoUsd = document.getElementById('usd')
+  precoUsd.innerHTML = 'U$' + btcLastPrice
 
   // Mais alto
 
   var precoMaisAlto = parseFloat(obj.highPrice).toFixed(2);
 
   let MaisAlto = document.getElementById("maisAlto")
-  MaisAlto.innerHTML = 'R$' + (precoMaisAlto * 5).toFixed(2)
+  MaisAlto.innerHTML = 'R$' + (precoMaisAlto * 5.35).toFixed(2)
 
   let MaisAltoR = document.getElementById("maisAlto-r")
-  MaisAltoR.innerHTML = 'R$' + (precoMaisAlto * 5).toFixed(2)
+  MaisAltoR.innerHTML = 'R$' + (precoMaisAlto * 5.35).toFixed(2)
 
   // Mais Baixo
 
   var precoMaisBaixo = parseFloat(obj.lowPrice).toFixed(2);
   
   let MaisBaixo = document.getElementById("maisBaixo")
-  MaisBaixo.innerHTML = 'R$' + (precoMaisBaixo * 5).toFixed(2)
+  MaisBaixo.innerHTML = 'R$' + (precoMaisBaixo * 5.35).toFixed(2)
 
   let MaisBaixoR = document.getElementById("maisBaixo-r")
-  MaisBaixoR.innerHTML = 'R$' + (precoMaisBaixo * 5).toFixed(2)
+  MaisBaixoR.innerHTML = 'R$' + (precoMaisBaixo * 5.35).toFixed(2)
 
   // volume    
   var volume = parseFloat(obj.volume).toFixed(2);
