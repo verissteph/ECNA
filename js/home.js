8f4d15c2b3fad93efd00e6dcbb25adc3258c0a65
Rodrigo <rodrigocosta47@outlook.com>
2020-07-06 23:18:33 -0300

    Adicionei ao código a api que vamso usar para consimo dos dodos em em reias, comeentei o codigo para ter uma maior clareza de cada etapa

--- js/home.js
+++ js/home.js
@@ -1,63 +1,24 @@
-var burl = 'https://api.binance.com';
-var query = '/api/v1/ticker/24hr';
+// Nesta parte eu uso set interval para atulizar a rquisições
+// Api que tras dados em Real
+setInterval(function(){
+  // No fetch eu passo o link da api
+  fetch("https://www.mercadobitcoin.net/api/BTC/ticker/")
+  .then(function(response){
+      return response.json();
+  })
+  .then(function(data){
+      // aqui acesso dos dados da api e passo para uma variavel
+      let ultimovalor = data.ticker.last
+      // aqui acesso o h1 atraves de u id que adicionei no html
+      let textoCotacao = document.getElementById("precobtcbrl")
+      // Aqui eu passo a mesma informação para o responsivo ultilizando o mesmo procedimento
+      let textoCotacaoR = document.getElementById("precobtcbrl-r")
 
-query += '?symbol=BTCUSDT';
+      // Por ultimo eu adiciono esse elementos chamados na API pra dentro do texto
+      textoCotacao.innerHTML = "R$" + parseFloat(ultimovalor).toFixed(2)
+      // Aqui eu passo a mesma informação para o responsivo ultilizando o mesmo procedimento
+      textoCotacaoR.innerHTML = "R$" + parseFloat(ultimovalor).toFixed(2)
+  })
+  // Ele faz a requisição na API a cada 1s
+}, 1000)
 
-var url = burl + query;
-
-var ourRequest = new XMLHttpRequest();
-
-ourRequest.open('GET',url,true);
-
-ourRequest.onload = function () {
-  const obj = JSON.parse(ourRequest.responseText);
-  var btcLastPrice = parseFloat(obj.lastPrice).toFixed(2);
-
-  // preco
-
-  let preco = document.getElementById("precobtcbrl")
-  preco.innerHTML = 'R$' + (btcLastPrice * 5.35).toFixed(2)
-  
-  let precoRespontivo = document.getElementById('precobtcbrl-r')
-  precoRespontivo.innerHTML = 'R$' + (btcLastPrice * 5.35).toFixed(2)
-
-  let precoUsd = document.getElementById('usd')
-  precoUsd.innerHTML = 'U$' + btcLastPrice
-
-  // Mais alto
-
-  var precoMaisAlto = parseFloat(obj.highPrice).toFixed(2);
-
-  let MaisAlto = document.getElementById("maisAlto")
-  MaisAlto.innerHTML = 'R$' + (precoMaisAlto * 5.35).toFixed(2)
-
-  let MaisAltoR = document.getElementById("maisAlto-r")
-  MaisAltoR.innerHTML = 'R$' + (precoMaisAlto * 5.35).toFixed(2)
-
-  // Mais Baixo
-
-  var precoMaisBaixo = parseFloat(obj.lowPrice).toFixed(2);
-  
-  let MaisBaixo = document.getElementById("maisBaixo")
-  MaisBaixo.innerHTML = 'R$' + (precoMaisBaixo * 5.35).toFixed(2)
-
-  let MaisBaixoR = document.getElementById("maisBaixo-r")
-  MaisBaixoR.innerHTML = 'R$' + (precoMaisBaixo * 5.35).toFixed(2)
-
-  // volume    
-  var volume = parseFloat(obj.volume).toFixed(2);
-  
-  let volumeElemeto = document.getElementById("volume")
-  volumeElemeto.innerHTML = 'U$' + volume
-  
-  let volumeElemetoR = document.getElementById("volume-r")
-  volumeElemetoR.innerHTML = 'U$' + volume
-
-}
-
-ourRequest.send()
-
-setInterval(function() {
-    ourRequest.open('GET',url,true);
-    ourRequest.send()
-}, 1000);
