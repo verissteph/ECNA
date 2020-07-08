893675e89b7b52c8812dea76de89f5e7f3029ab4
wborba195 <willianborbadasilva@yahoo.com.br>
2020-07-08 00:36:05 -0300

    API cotacao Dolar + conversao volume (Will)

--- js/home.js
+++ js/home.js
@@ -1,4 +1,23 @@
 // Nesta parte eu uso set interval para atulizar a rquisições
+// API que tras a cotacao do dolar para reais
+setInterval(function(){
+  fetch("https://economia.awesomeapi.com.br/all/USD-BRL")
+  .then(function(response){
+    return response.json()
+  })
+  .then(function(data){
+    let cotacaoUsdBrl = data.USD.high
+    let cotacao = cotacaoUsdBrl
+    let cotacaoUsd = document.getElementById("cotacaoUsd")
+    cotacaoUsd.innerHTML = parseFloat(cotacao)
+    //console.log(cotacao)
+  })
+  .catch(function(error){
+    console.log(error)
+  })
+},1000)
+
+
 // Api que tras dados em Real
 setInterval(function(){
   // No fetch eu passo o link da api
@@ -12,34 +31,24 @@ setInterval(function(){
       let volumedia = data.ticker.vol
       let valorMaxFinal = data.ticker.high
       let valorMinFinal = data.ticker.low
-      // esquema para conseguir quebrar a parte inteira e a parte decimal para o responsivo (Will)
-      let volumedia_split = volumedia.split(".")
+
       // aqui acesso o h1 atraves de u id que adicionei no html
       let textoCotacao = document.getElementById("precobtcbrl")
       // Aqui eu passo a mesma informação para o responsivo ultilizando o mesmo procedimento
       let textoCotacaoR = document.getElementById("precobtcbrl-r")
-      // Aqui acesso o em do volume transacional no dia (will)
-      let textoVolume = document.getElementById("volume")
-      let textoVolumeR = document.getElementById("volume-r")
-
+      
       //valor maximo na variação - ellen
       let maximoValor = document.querySelector('#maisAlto')
        maximoValor.innerText = "R$" + parseFloat(valorMaxFinal).toFixed(2);
-
+      
        //valor minimo na variação - ellen
       let minimoValor = document.querySelector('#maisBaixo')
       minimoValor.innerHTML = "R$" + parseFloat(valorMinFinal).toFixed(2);
 
-
       // Por ultimo eu adiciono esse elementos chamados na API pra dentro do texto
       textoCotacao.innerHTML = "R$" + parseFloat(ultimovalor).toFixed(2)
       // Aqui eu passo a mesma informação para o responsivo ultilizando o mesmo procedimento
-      textoCotacaoR.innerHTML = "R$" + parseFloat(ultimovalor).toFixed(2)
-      // Atribuição do valor da API no volume (will)
-      textoVolume.innerHTML = "R$" + parseFloat(volumedia).toFixed(2)
-      //Responsivo (will)
-      textoVolumeR.innerHTML = "R$" + volumedia_split[0] + "<span>," + volumedia_split[1].substring(0,2) + "</span>"
-      
+      textoCotacaoR.innerHTML = "R$" + parseFloat(ultimovalor).toFixed(2)         
   })
   //Adicionando trecho que verifica, em caso de erro no vínculo com a API, qual o tipo do erro (will)
   .catch(function(error){
@@ -47,7 +56,6 @@ setInterval(function(){
   })
   // Ele faz a requisição na API a cada 1s
 }, 1000)
-
 
 
 // Aqui usei a api que tras os dados em dollar
@@ -59,14 +67,28 @@ setInterval(function(){
   .then(function(data){
       let ultimovalor = data.lastPrice
       let volumediausd = data.volume
+      // esquema para conseguir quebrar a parte inteira e a parte decimal para o responsivo (Will)
       let volumediausd_split = volumediausd.split(".")
       let textoCotacaoUsd = document.getElementById("usd")
       let textoCotacaoUsdR = document.getElementById("usd-r")
+      // Aqui acesso as tags do volume transacional no dia (will)
       let textoVolumeDiaUsdR = document.getElementById("volumeusd-r")
+      let textoVolume = document.getElementById("volume")
+      let textoVolumeR = document.getElementById("volume-r")
+      let cotacaoUsd2 = document.getElementById("cotacaoUsd").innerText
+      let volumeReais = parseFloat(cotacaoUsd2)*parseFloat(volumediausd)
+      //console.log(volumeReais)
+      // esquema para conseguir quebrar a parte inteira e a parte decimal para o responsivo (Will)
+      let volumeReais_str = volumeReais.toString().split(".")
+      //console.log(volumeReais_str)
 
       textoCotacaoUsd.innerHTML = "U$" + parseFloat(ultimovalor).toFixed(2)
       textoCotacaoUsdR.innerHTML = "U$" + parseFloat(ultimovalor).toFixed(2)
       textoVolumeDiaUsdR.innerHTML = "U$" + volumediausd_split[0] + "<span>," + volumediausd_split[1].substring(0,2) + "</span>"
+      // Atribuição do valor da API no volume (will)
+      textoVolume.innerHTML = "R$" + volumeReais.toFixed(2)
+      //Responsivo (will)
+      textoVolumeR.innerHTML = "R$" + volumeReais_str[0] + "<span>," + volumeReais_str[1].substring(0,2) + "</span>"
 
   })
   .catch(function(error){
