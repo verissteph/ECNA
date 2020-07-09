fc6b033c159842ed15477f3dbb442ec00883472f
verissteph <stephanieverissimopp@gmail.com>
2020-07-08 21:24:50 -0300

    cont. testes netlify. Criando arq separados jquery

--- cadastro/js/cadastro.js
+++ cadastro/js/cadastro.js
@@ -24,21 +24,4 @@ formulario.addEventListener("submit", function (e) {
     }
 });
 
-$(document).ready(function ($) {
-    const $formulario = $("#formulario");
-    
-    $formulario.submit(e => {
-      e.preventDefault();
-      const $action = $formulario.attr('action');
-      const $data = $formulario.serialize();
-      $.post($action, $data).then(() => {
-        Swal.fire({
-            icon: 'success',
-            title: 'Mensagem Enviada!',
-            text: 'Muito obrigado!',
-            footer: '<a href="/index.html">Voltar para home</a>'
-        });
-      });
-    });
 
-  });
