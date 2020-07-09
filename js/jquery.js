056776c05d9000a7ce191b6ee1952bbb9ffb9feb
verissteph <stephanieverissimopp@gmail.com>
2020-07-08 22:15:19 -0300

    inclusao e criação arquivo jquery para contatos

--- js/jquery.js
+++ js/jquery.js
@@ -16,3 +16,21 @@ $(document).ready(function ($) {
     });
 
   });
+$(document).ready(function ($) {
+    const $formulario = $("#contato");
+    
+    $formulario.submit(e => {
+      e.preventDefault();
+      const $action = $formulario.attr('action');
+      const $data = $formulario.serialize();
+      $.post($action, $data).then(() => {
+        Swal.fire({
+            icon: 'success',
+            title: 'Mensagem Enviada!',
+            text: 'Muito obrigado!',
+            footer: '<a href="/index.html">Voltar para home</a>'
+        });
+      });
+    });
+
+  });
